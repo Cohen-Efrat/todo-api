fb9b7344bb6618e75dcfb73789ef40196cf89036
אפרת כהן <efratc66@gmail.com>
2019-01-28 21:12:52 +0200

    fiexd connaction to DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,11 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
 
-mongoose.connect('mongodb://<efrat_c>:<ec1992ec>@ds211865.mlab.com:11865/todo_app');
+mongoose.connect('mongodb://admin:qwerty123@ds211865.mlab.com:11865/todo_app',{ useNewUrlParser: true }).then((e)=>{
+
+},(e)=>{
+  console.log(e);
+});
 
 
 module.exports = {mongoose};
