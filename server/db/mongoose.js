4acd4ff784c6b79cb9f8e277137dea2c151d7a6f
אפרת כהן <efratc66@gmail.com>
2019-01-28 20:59:43 +0200

    fixed syntx

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://<efrat_c>:<ec1992ec>@ds211865.mlab.com:11865/todo_app' || 'mongodb://localhost:27017/TodoApp').then((a)=>{
   console.log(a);
-  ,(e)=>{
+},(e)=>{
     console.log(e);
   }
 });
